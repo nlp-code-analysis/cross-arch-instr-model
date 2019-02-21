1129deb529f62c2f9994244950cbb6e8cbb3e302
nlp-code-analysis <46032101+nlp-code-analysis@users.noreply.github.com>
2019-02-21 14:05:05 -0500

    Update senvec.py

--- senvec.py
+++ senvec.py
@@ -133,44 +133,10 @@ def calculateSentenceVectors(testset, dim, dictry):
 
     return finalout
 
-# calculate basic block vectors using hyperbolic tangent
-def calculateTanhVectors(testset, dim, dictry):
-    finalout = []
-    for bb in testset:
-        senvec = numpy.zeros([1, int(dim)])
-        count = 0
-
-        for word in bb:
-            if word in dictry:
-
-                if count % 2 == 1:
-                    wordi1 = dictry[word]
-                else:
-                    wordi2 = dictry[word]
-
-                if count > 0:
-                    pair = wordi1 + wordi2
-                    senvec += numpy.tanh(pair)
-    
-                count += 1
-            
-            # else: don't count words not in dictionary
-
-        # cannot use all zeros to compute cos similarity
-        if numpy.all(senvec == 0.0):
-            senvec.fill(0.1)
-
-        finalout.append(senvec)
-
-    return finalout
 
 print("Calculating sentence vectors...")
 bbfinal_a1 = calculateSentenceVectors(armtest, dim, armdict1)
 bbfinal_x1 = calculateSentenceVectors(x86test, dim, x86dict1)
-
-print("Calculating tanh sentence vectors...")
-bbfinal_at = calculateTanhVectors(armtest, dim, armdict1)
-bbfinal_xt = calculateTanhVectors(x86test, dim, x86dict1)
 
 
 ###                 ###
@@ -203,68 +169,14 @@ def ROC(bbfinal_a, bbfinal_x):
     return metrics.roc_curve(true_labels, cos_sim, pos_label=1)
 
 
-# method to calculate Manhattan distance
-def ROC_M(bbfinal_a, bbfinal_x):
-    man_sim = []
-    for bb1, bb2 in zip(bbfinal_a, bbfinal_x):
-        sim = 1 - spatial.distance.cityblock(bb1, bb2)
-        man_sim.append(sim)
-
-    return metrics.roc_curve(true_labels, man_sim, pos_label=1)
-
-
-# method to calculate Euclidean distance
-def ROC_E(bbfinal_a, bbfinal_x):
-    euc_sim = []
-    for bb1, bb2 in zip(bbfinal_a, bbfinal_x):
-        sim = 1 - numpy.linalg.norm(bb1-bb2)
-        euc_sim.append(sim)
-
-    return metrics.roc_curve(true_labels, euc_sim, pos_label=1)
-
 print("Calculating similarity scores, ROC...")
 fpr1, tpr1, thresholds1 = ROC(bbfinal_a1, bbfinal_x1)
-fpr2, tpr2, thresholds2 = ROC_M(bbfinal_a1, bbfinal_x1)
-fpr3, tpr3, thresholds3 = ROC_E(bbfinal_a1, bbfinal_x1)
-
-fprt, tprt, thresholdst = ROC(bbfinal_at, bbfinal_xt)
-fprt2, tprt2, thresholdst2 = ROC_M(bbfinal_at, bbfinal_xt)
-fprt3, tprt3, thresholdst3 = ROC_E(bbfinal_at, bbfinal_xt)
-
 auc1 = metrics.auc(fpr1, tpr1)
-auc2 = metrics.auc(fpr2, tpr2)
-auc3 = metrics.auc(fpr3, tpr3)
-
-auct = metrics.auc(fprt, tprt)
-auct2 = metrics.auc(fprt2, tprt2)
-auct3 = metrics.auc(fprt3, tprt3)
-
 print("AUC avg cos: %f" % auc1)
-print("AUC avg man: %f" % auc2)
-print("AUC avg euc: %f" % auc3)
-
-print("AUC tanh cos: %f" % auct)
-print("AUC tanh man: %f" % auct2)
-print("AUC tanh euc: %f" % auct3)
 
 
 plt.title('ARM-X86 Basic Block Similarity')
 plt.plot(fpr1, tpr1, label="Cosine Similarity, AUC=%f" %auc1)
-plt.plot(fpr2, tpr2, label="Manhattan Distance, AUC=%f" %auc2)
-plt.plot(fpr3, tpr3, label="Euclidean Distance, AUC=%f" %auc3)
-
-plt.legend(loc = 'lower right')
-plt.plot([0,1], [0,1], 'r--')
-plt.xlim([0,1])
-plt.ylim([0,1])
-plt.ylabel('True Positive Rate')
-plt.xlabel('False Positive Rate')
-plt.show()
-
-plt.title('ARM-X86 Basic Block Similarity (Tanh)')
-plt.plot(fprt, tprt, label="Cosine Similarity, AUC=%f" %auct)
-plt.plot(fpr2, tpr2, label="Manhattan Distance, AUC=%f" %auct2)
-plt.plot(fpr3, tpr3, label="Euclidean Distance, AUC=%f" %auct3)
 
 plt.legend(loc = 'lower right')
 plt.plot([0,1], [0,1], 'r--')
